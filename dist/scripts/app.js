4a072aff7bea0898e6222a62a72b48033acaf341
emikod@mac.com <emikod@mac.com>
2016-11-01 15:25:10 -0600

    Adds routing for landing and album views

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,22 @@
-angular.module('musicJams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+      $locationProvider
+          .html5Model({
+              enabled: true,
+              requireBase: false
+          });
+
+      $stateProvider
+          .state('landing', {
+              url: '/',
+              templateUrl: '/templates/landing.html'
+          })
+          .state('album', {
+              url: '/album',
+              templateUrl: '/templates/album.html'
+          });
+  }
+  angular
+      .module('musicJams', ['ui.router'])
+      .config(config);
+})();
