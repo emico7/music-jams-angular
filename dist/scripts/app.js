38221cc39de5d5dfc82119df0d0146472731de44
emikod@mac.com <emikod@mac.com>
2016-11-03 14:18:41 -0600

    Add player-bar template, adds it to album view

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,24 +1,24 @@
 (function() {
-  function config($stateProvider, $locationProvider) {
-      $locationProvider
-          .html5Model({
-              enabled: true,
-              requireBase: false
-          });
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Model({
+                enabled: true,
+                requireBase: false
+            });
 
-      $stateProvider
-          .state('landing', {
-              url: '/',
-              templateUrl: '/templates/landing.html'
-          })
-          .state('album', {
-              url: '/album',
-              templateUrl: '/templates/album.html'
-          })
-          .state('collection', {
-              url: '/collection',
-              templateUrl: '/templates/collection.html'
-          })
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
+            });
   }
   angular
       .module('musicJams', ['ui.router'])
