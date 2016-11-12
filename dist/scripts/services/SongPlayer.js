72f651260fb900b15722280883d5ebe58d808d2c
emikod@mac.com <emikod@mac.com>
2016-11-12 16:21:51 -0700

    playSong function

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,17 +1,18 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
+
+        // @desc Song that is currently playing or on pause
+        // @type {Object}
         var currentSong = null;
 
         // @desc Buzz object audio file
         // @type {Object}
-
         var currentBuzzObject = null;
 
         // @function setSong
         // @desc Stops currently playing song and loads new audio file as currentBuzzObject
         // @param {Object} song
-
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -26,7 +27,9 @@
             currentSong = song;
         };
 
-
+        // @function playSong
+        // @desc Start playing song and song state to indicate that it is currently playing
+        // @param {Object} song
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
@@ -35,7 +38,7 @@
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                playSong();
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
