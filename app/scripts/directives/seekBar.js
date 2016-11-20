ffc77edddf06341bc08e6704ef22467e9dd31029
emikod@mac.com <emikod@mac.com>
2016-11-19 23:37:28 -0700

    Makes seek bar responsive to currently playing song

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,12 +14,22 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
 
                 var seekBar = $(element);
+
+                attributes.$observe('value', function(newValue) {
+                    scope.value = newValue;
+                });
+
+                attributes.$observe('max', function(newValue) {
+                    scope.max = newValue;
+                });
 
                 var percentString = function () {
                         var value = scope.value;
@@ -29,12 +39,17 @@
                 };
 
                 scope.fillStyle = function() {
-                        return {width: percentString()};
+                    return {width: percentString()};
+                };
+
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
                 };
 
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
 
                 scope.trackThumb = function() {
@@ -42,6 +57,7 @@
                         var percent = calculatePercent(seekBar, event);
                         scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.vlaue);
                         });
                     });
 
@@ -51,8 +67,10 @@
                     });
                 };
 
-                scope.thumbStyle = function() {
-                    return {left: percentString()};
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
                 };
             }
         };
