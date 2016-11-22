70bf8b2047395de671483145499391ba3f3da399
emikod@mac.com <emikod@mac.com>
2016-11-22 11:40:26 -0700

    Changes song duration format

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -9,11 +9,11 @@
             year: '1881',
             albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-                { title: 'Blue', duration: '161:71', audioUrl: '/assets/music/blue' },
-                { title: 'Green', duration: '103:96', audioUrl: '/assets/music/green' },
-                { title: 'Red', duration: '268:45', audioUrl: '/assets/music/red' },
-                { title: 'Pink', duration: '153:14', audioUrl: '/assets/music/pink' },
-                { title: 'Magenta', duration: '374:22', audioUrl: '/assets/music/magenta' }
+                { title: 'Blue', duration: '161.71', audioUrl: '/assets/music/blue' },
+                { title: 'Green', duration: '103.96', audioUrl: '/assets/music/green' },
+                { title: 'Red', duration: '268.45', audioUrl: '/assets/music/red' },
+                { title: 'Pink', duration: '153.14', audioUrl: '/assets/music/pink' },
+                { title: 'Magenta', duration: '374.22', audioUrl: '/assets/music/magenta' }
             ]
         };
 
@@ -24,11 +24,11 @@
             year: '1909',
             albumArtUrl: '/assets/images/album_covers/20.png',
             songs: [
-                { title: 'Hello, Operator?', duration: '1:01' },
-                { title: 'Ring, ring, ring', duration: '5:01' },
-                { title: 'Fits in your poket', duration: '3:21' },
-                { title: 'Can you hear me now?', duration: '3:14' },
-                { title: 'Magenta', duration: '2:15' }
+                { title: 'Hello, Operator?', duration: '1.01' },
+                { title: 'Ring, ring, ring', duration: '5.01' },
+                { title: 'Fits in your poket', duration: '3.21' },
+                { title: 'Can you hear me now?', duration: '3.14' },
+                { title: 'Magenta', duration: '2.15' }
             ]
         };
 
